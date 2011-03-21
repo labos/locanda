79700b3c7dea466ec10bbf1f57959a1bc22fe571
guido <guido@5a08f537-7b09-473d-a1d1-551e4afdb7c2>
2011-03-21 15:02:00 +0000

    Bugfix sulla GuestAction.
    git-svn-id: svn://93.63.174.84:8080/repos/locanda/trunk@525 5a08f537-7b09-473d-a1d1-551e4afdb7c2

--- src/action/GuestAction.java
+++ src/action/GuestAction.java
@@ -9,13 +9,16 @@ import model.Structure;
 import model.User;
 import model.internal.Message;
 
+
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+@ParentPackage(value="default")
 public class GuestAction extends ActionSupport implements SessionAware{
 	private Map<String, Object> session = null;
 	private List<Guest> guests = null;
