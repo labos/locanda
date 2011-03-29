50a67ef04efc99957e58f7f4ff02a0df0e9054af
alberto <alberto@5a08f537-7b09-473d-a1d1-551e4afdb7c2>
2011-03-29 15:42:24 +0000

    aggiunta codice
    git-svn-id: svn://93.63.174.84:8080/repos/locanda/trunk@587 5a08f537-7b09-473d-a1d1-551e4afdb7c2

--- src/action/SeasonAction.java
+++ src/action/SeasonAction.java
@@ -1,5 +1,9 @@
 package action;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -11,27 +15,71 @@ import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import model.Booking;
 import model.Structure;
 import model.User;
+import model.internal.Message;
+import model.listini.Period;
 import model.listini.Season;
 @ParentPackage(value="default")
 public class SeasonAction extends ActionSupport implements SessionAware{
 	private Map<String, Object> session = null;
 	private List<Season> seasons = null;
+	private Season season = null;
 	private String startDate = null;
 	private String endDate = null;
-	
+	private Message message = new Message();
 
 
 	@Actions({
 		@Action(value="/goAddNewSeason",results = {
-				@Result(name="success",location="/seasons.jsp")
-				})			
+				@Result(type ="json",name="success", params={
+						"root","message"
+				} ),
+				@Result(name="input", location="/validationError.jsp"),
+				@Result(type ="json",name="error", params={
+						"root","message"
+				} )
+		}	)		
 				})
 				
 	public String goAddNewSeason(){
+		User user = (User)session.get("user");
+		Structure structure = user.getStructure();
+		this.setSeasons(structure.getSeasons());
 		
+		if (!this.saveUpdatePeriodDates())
+		{
+			this.getMessage().setResult(Message.ERROR);
+			this.getMessage().setDescription("Problema ai dati della season!");
+			return ERROR;
+			
+		}
+		
+		Season oldSeason = 
+			structure.findSeasonById(this.getSeason().getId());
+		if(oldSeason==null){
+			//Si tratta di una nuova season
+			this.getSeason().setId(structure.nextKey());
+			structure.addSeason(this.getSeason());
+		}else{
+			//Si tratta di un update di un booking esistente
+			structure.updateSeason(this.getSeason());
+			
+		}
+
+		this.getMessage().setResult(Message.SUCCESS);
+		this.getMessage().setDescription("Season Added successfully");
 		return SUCCESS;
+
+	}
+
+	public Message getMessage() {
+		return message;
+	}
+
+	public void setMessage(Message message) {
+		this.message = message;
 	}
 
 	@Actions({
@@ -70,6 +118,37 @@ public class SeasonAction extends ActionSupport implements SessionAware{
 	public String findSeasonByPeriod(){
 		
 		return SUCCESS;
+	}
+	
+	public Season getSeason() {
+		return season;
+	}
+
+	public void setSeason(Season season) {
+		this.season = season;
+	}
+
+	
+	private Boolean saveUpdatePeriodDates(){
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+		Date dateOut = null;
+		Long millis = null;
+		Period aPeriod = new Period();
+		List <Period> aPeriods = new ArrayList<Period>();
+		
+		try {
+			aPeriod.setStartDate(sdf.parse(this.getStartDate()));
+			aPeriod.setEndDate(sdf.parse(this.getEndDate()));
+			aPeriods.add(aPeriod);
+			this.getSeason().setPeriods(aPeriods);			
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return false;
+		}
+			
+		
+		return true;
+		
 	}
 	
 	public Map<String, Object> getSession() {
