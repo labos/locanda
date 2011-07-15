f72f1c0b691d1d15b158a2bf4b6bd100558dc416
alberto <alberto@5a08f537-7b09-473d-a1d1-551e4afdb7c2>
2011-07-15 15:46:24 +0000

    downloading
    git-svn-id: svn://93.63.174.84:8080/repos/locanda/trunk@1025 5a08f537-7b09-473d-a1d1-551e4afdb7c2

--- src/action/DownloadAction.java
+++ src/action/DownloadAction.java
@@ -1,91 +1,62 @@
 package action;
 
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+
+import javax.servlet.ServletContext;
+
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 @ParentPackage(value = "default")
-public class DownloadAction extends ActionSupport {
-	private InputStream inputStream;
-	private String name;
-	private String surname;
-	private String email;
-	private String phone;
+public class DownloadAction extends ActionSupport{
 
+	private InputStream fileInputStream;
+	private String fileName = null;
+	
 	@Actions(
-		@Action(value = "/goDownload", results = {
-				@Result(name = "success", location = "/download.jsp") 
-				}) 
-			)
-	public String goDownload() {
+			@Action(value = "/goDownload", results = {
+					@Result(name = "success", location = "/download.jsp") 
+					}) 
+				)
+		public String goDownload() {
 
-		return SUCCESS;
-	}
-
-	@Actions(
-	@Action(value = "/download", results = {
-				@Result(name = "success", type = "stream", params = {
-					"inputName","inputStream", "contentType", "application/octet-stream"}) 
-					})
-		)
-	public String download() {
-		String fileName = "/locanda.zip";
-		
-		 try {
-			this.setInputStream(new FileInputStream(new File(fileName)));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return SUCCESS;
 		}
 
-		return SUCCESS;
+
+	public String execute() throws Exception {
+		ServletContext context = null; 
+		String filePath = null;
+		context =  ServletActionContext.getServletContext();
+		this.setFileName("locanda.zip");
+		filePath = context.getRealPath("/") + this.getFileName();			
+		fileInputStream = new FileInputStream(new File(filePath ));
+
+	    return SUCCESS;
+	  
 	}
 
-	public String getName() {
-		return name;
+	public InputStream getFileInputStream() {
+		 
+		return fileInputStream;
+	   
+	}
+	
+	public String getFileName() {
+		return fileName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public InputStream getInputStream() {
-		return this.inputStream;
-	}
-
-	public void setInputStream(InputStream inputStream) {
-		this.inputStream = inputStream;
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
 	}
 
 	
