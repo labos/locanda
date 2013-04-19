a8fe683967f2b828d903277c18baa25bf50831a8
guido <guido@labos01>
2013-04-19 12:04:38 +0200

    [REST] TourismType and Transport resources
    -TourismType and Transport GET method (findAll)

---
+++ src/resources/TourismTypeResource.java
@@ -0,0 +1,41 @@
+package resources;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.questura.TourismType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import service.TourismTypeService;
+
+@Path("/tourismTypes/")
+@Component
+@Scope("prototype")
+public class TourismTypeResource {
+	@Autowired
+    private TourismTypeService tourismTypeService = null;
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<TourismType> findAll(){
+    	List<TourismType> ret = null;
+    	
+    	ret = this.getTourismTypeService().findAll();
+        return ret;
+    }
+
+	public TourismTypeService getTourismTypeService() {
+		return tourismTypeService;
+	}
+	public void setTourismTypeService(TourismTypeService tourismTypeService) {
+		this.tourismTypeService = tourismTypeService;
+	}
+
+}
---
+++ src/resources/TransportResource.java
@@ -0,0 +1,41 @@
+package resources;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.questura.Transport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import service.TransportService;
+
+@Path("/transports/")
+@Component
+@Scope("prototype")
+public class TransportResource {
+	@Autowired
+    private TransportService transportService = null;
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Transport> findAll(){
+    	List<Transport> ret = null;
+    	
+    	ret = this.getTransportService().findAll();
+        return ret;
+    }
+
+	public TransportService getTransportService() {
+		return transportService;
+	}
+	public void setTransportService(TransportService transportService) {
+		this.transportService = transportService;
+	}
+
+}
