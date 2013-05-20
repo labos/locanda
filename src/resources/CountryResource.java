ef5f509e0dc381ed673a58a028cff03a2cdaa87c
guido <guido@labos01>
2013-05-20 16:48:22 +0200

    [I18n] Country localization
    -The table "Country" has now the country codes as labels
    -Properties translated for countries in IT and EN

--- src/resources/CountryResource.java
+++ src/resources/CountryResource.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import service.CountryService;
+import utils.I18nUtils;
 
 @Path("/countries/")
 @Component
@@ -28,6 +29,10 @@ public class CountryResource {
     	List<Country> ret = null;
     	
     	ret = this.getCountryService().findAll();
+    	
+    	for(Country aCountry : ret){
+    		aCountry.setDescription(I18nUtils.getProperty(aCountry.getDescription()));
+    	}
         return ret;
     }
 
